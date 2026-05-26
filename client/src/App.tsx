2070f8572f824b48ba8f6ae0b1a7b692d33eb3f0
AMWebDesign-ar <claudio.mehrbald@gmail.com>
2026-05-26 14:35:57 -0300

    modified:   client/src/App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,21 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
+
 import Home from "@/pages/Home";
+import NotFound from "@/pages/not-found";
 import ManageBooking from "@/pages/ManageBooking";
 import AdminLogin from "@/pages/AdminLogin";
 import AdminDashboard from "@/pages/AdminDashboard";
 
-function Router() {
+export default function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/reserva/:token" component={ManageBooking} />
-      <Route component={NotFound} />
+
       <Route path="/admin/login" component={AdminLogin} />
       <Route path="/admin" component={AdminDashboard} />
+
+      <Route component={NotFound} />
     </Switch>
   );
 }
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
-
-export default App;
